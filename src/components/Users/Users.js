a5f7cf6c52b5b0a9b51b2f2120ca3ae6ef32606c
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-30 05:17:59 +0600

    user delete feature added

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,6 +4,24 @@ import { useEffect } from 'react';
 
 const Users = () => {
     const [users,setUsers]=useState([]);
+    const handleDelete = (id)=>{
+        const proceed = window.confirm('Do you want to delete?');
+        if(proceed){
+            const url =  `http://localhost:5000/users/${id}`
+        fetch(url,{
+            method: 'DELETE'
+        })
+        .then(res =>res.json())
+        .then(data=>{
+            if(data.deletedCount>0){
+                alert('delete successfullu');
+                const remainingUsers = users.filter(user => user._id!==id);
+                setUsers(remainingUsers);
+            }
+        })
+        }
+
+    }
     useEffect(()=>{
         fetch('http://localhost:5000/users')
         .then(res=>res.json())
@@ -16,7 +34,7 @@ const Users = () => {
             <h2>Users Available {users.length}</h2>
             <ul>
                {
-                   users.map(user=><li key={user._id}>{user.name} : {user.email}  </li>)
+                   users.map(user=><li key={user._id}>{user.name} : {user.email} <button>Update</button> <button onClick={()=>handleDelete(user._id)}>X</button>  </li>)
                }
             </ul>
         </div>
