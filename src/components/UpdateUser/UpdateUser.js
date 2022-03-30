9cdd86c372a767a41969cbead980b7985ca76776
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-30 06:21:37 +0600

    load single item

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const UpdateUser = () => {
+    const {id} =useParams();
+    const [user,setUser]=useState({});
+    useEffect(()=>{
+        fetch(`http://localhost:5000/users/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            setUser(data);
+        })
+    },[])
+    console.log(id);
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update User</h2>
+            <h3>{user.name} : {user.email}</h3>
         </div>
     );
 };
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => {
     const [users,setUsers]=useState([]);
@@ -34,7 +35,11 @@ const Users = () => {
             <h2>Users Available {users.length}</h2>
             <ul>
                {
-                   users.map(user=><li key={user._id}>{user.name} : {user.email} <button>Update</button> <button onClick={()=>handleDelete(user._id)}>X</button>  </li>)
+                   users.map(user=><li key={user._id}>{user.name} : {user.email}
+                     <Link to={`/users/update/${user._id}`}>
+                     <button>Update</button> 
+                     </Link>
+                     <button onClick={()=>handleDelete(user._id)}>X</button>  </li>)
                }
             </ul>
         </div>
